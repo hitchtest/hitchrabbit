6757f5e3cb32f01a8d8b77cca4fe3caa45786ce9
Colm O'Connor <colm.oconnor.github@gmail.com>
2015-09-03 21:23:09 +0800

    FEATURE : Specify rabbit virtual hosts, users and permissions for the service to start with.

--- hitchrabbit/__init__.py
+++ hitchrabbit/__init__.py
@@ -1,2 +1,5 @@
+from hitchrabbit.rabbit_service import RabbitVirtualHost
+from hitchrabbit.rabbit_service import RabbitUser
+from hitchrabbit.rabbit_service import RabbitPermission
 from hitchrabbit.rabbit_service import RabbitService
 from hitchrabbit.rabbit_package import RabbitPackage
--- hitchrabbit/rabbit_service.py
+++ hitchrabbit/rabbit_service.py
@@ -2,18 +2,58 @@ from hitchserve import Service
 from os.path import join
 import signal
 import shutil
+import socket
 import sys
+
+
+class RabbitUser(object):
+    def __init__(self, username, password, permissions=None):
+        self.username = username
+        self.password = password
+        self.permissions = permissions if permissions is not None else []
+
+class RabbitVirtualHost(object):
+    def __init__(self, name):
+        self.name = name
+
+class RabbitPermission(object):
+    def __init__(self, vhost, conf=None, write=None, read=None):
+        self.vhost = vhost
+        self.conf = conf if conf is not None else ".*"
+        self.write = write if write is not None else ".*"
+        self.read = read if read is not None else ".*"
 
 
 class RabbitService(Service):
     stop_signal = signal.SIGTERM
 
-    def __init__(self, rabbit_package, **kwargs):
+    def __init__(self, rabbit_package, initialize=True, vhosts=None, users=None, **kwargs):
         self.rabbit_package = rabbit_package
         self._datadir = None
+        self.users = users
+        self.vhosts = vhosts
+        self.initialize = initialize
         kwargs['command'] = [rabbit_package.server, ]
         kwargs['log_line_ready_checker'] = lambda line: "completed" in line
         super(RabbitService, self).__init__(**kwargs)
+
+    def setup(self):
+        """Remove rabbit data directory if set to initialize."""
+        if self.initialize:
+            shutil.rmtree(self.datadir, ignore_errors=True)
+
+    def poststart(self):
+        """Create rabbit users, vhosts and set permissions."""
+        if self.initialize:
+            for vhost in self.vhosts:
+                self.ctl("add_vhost", vhost.name).run()
+            for user in self.users:
+                self.ctl("add_user", user.username, user.password).run()
+                for permission in user.permissions:
+                    self.ctl(
+                        "set_permissions", "-p", permission.vhost.name,
+                        user.username, permission.conf, permission.write, permission.read
+                    ).run()
 
     @property
     def datadir(self):
@@ -31,6 +71,7 @@ class RabbitService(Service):
     def env_vars(self):
         env_vars = super(RabbitService, self).env_vars
         env_vars.update({
+            "HOSTNAME": "{}.local".format(socket.gethostname()),
             "RABBITMQ_LOG_BASE": self.datadir,
             "RABBITMQ_MNESIA_BASE": self.datadir,
         })
